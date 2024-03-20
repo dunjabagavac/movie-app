aded7406fabb2c7d769a6f3dcec581ff0dcbfdef
dunjabagavac <dunjabagavac00@gmail.com>
2024-03-20 10:00:53 +0100

    project setup

---
+++ src/env.js
@@ -0,0 +1,3 @@
+export default {
+  apiKey: '727809f0'
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,rs,jsx,tsx}'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      saira: ['Saira, sans-serif']
+    }
+  },
+  plugins: []
+}
